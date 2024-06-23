5785815b79af8b61496a3967ee276837bc9d89a2
Yeonb0 <arang02660@gmail.com>
2024-06-23 20:50:52 +0900

    new file:   C/Chapter-02/HelloComment.c new file:   C/Chapter-02/PrintfOne.c 	new file:   C/Chapter-02/PrintfTwo.c 	new file:   C/Chapter-02/Question2.c 	new file:   C/Chapter-02/Question3.c 	new file:   C/Chapter-02/Question4.c

---
+++ C/Chapter-02/HelloComment.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*
+제목 : Hello world 출력하기
+기능 : 문자열 출력
+파일이름: HelloComment.c
+수정날짜: 2024. 06. 23
+작성자 : 김보연
+*/
+#include <stdio.h> // 헤더파일 선언
+
+int main(){  // main 함수의 시작
+  /* 
+  이 함수 내에서는 하나의 문자열을 출력한다.
+  문자열은 모니터로 출력된다.
+  */
+  printf("Hello world\n");  // 문자열 출력
+  return 0;  // 0의 반환
+} // main 함수의 끝
---
+++ C/Chapter-02/Question2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+  printf("이름 : 김보연\n");
+  printf("나이 : 22\n");
+  printf("주소 : 경기도 김포시\n");
+  printf("전화번호 : 010-XXXX-XXXX");
+
+  return 0;
+}
