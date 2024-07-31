52a669a1be96f631753ca74da205c58d56b3f45a
Yeonb0 <arang02660@gmail.com>
2024-07-31 14:52:09 +0900

    Changes to be committed:

--- Java/Chapter-04/RelationalOp.java
+++ Java/Chapter-04/RelationalOp.java
@@ -2,7 +2,7 @@ public class RelationalOp {
   public static void main(String[] args) {
     System.out.println("3 >= 2 : " + (3 >= 2));
     System.out.println("3 <= 2 : " + (3 <= 2));
-    System.out.println("7.0 == 7 : " + (7.0 == 7));
-    System.out.println("7.0 != 7 : " + (7.0 != 7));
+    System.out.println("7.0 == 7 : " + (7.0 == 6));
+    System.out.println("7.0 != 7 : " + (7.0 != 6));
   }
 }
